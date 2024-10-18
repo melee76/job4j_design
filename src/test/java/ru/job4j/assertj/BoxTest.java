ea4763a09266c41d3a52b55061d5fbe94ee8277f
zoubarevna <melee76@yandex.ru>
2024-10-18 12:28:34 +0300

    3. Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere")
+                .isNotEmpty();
+    }
+    @Test
+    void howManyVertexes() {
+        Box box = new Box(4,5);
+        int count = box.getNumberOfVertices();
+        assertThat(count).isEqualTo(4)
+                .isGreaterThan(0);
+    }
+    @Test
+    void checkBoolean () {
+        Box box = new Box(3,3);
+        boolean rsl = box.isExist();
+        assertThat(rsl).isTrue()
+                .isFalse();
+    }
+    @Test
+    void isGetArea() {
+        Box box = new Box(8,2);
+        double e = box.getArea();
+        assertThat(e).isPositive()
+                .isLessThan(25);
+    }
+}
