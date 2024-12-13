f753cb0078e754bf37e5744475cbbda16f7cd52a
zoubarevna <melee76@yandex.ru>
2024-12-13 12:02:18 +0300

    0. Что такое база данных. [#6860]

--- db/cars.sql
+++ db/cars.sql
@@ -1,2 +1,18 @@
+create table cars(
+	id serial primary key,
+	brand varchar(255),
+	model text,
+	price money 
+);
+
+insert into cars(brand, model, price) values('audi', 'a6', '40000');
+
+select * from cars;
+
+update cars set price = '38000';
+
+select * from cars;
+
 delete from cars;
+
 select * from cars;
